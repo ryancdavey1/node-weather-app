a10299b8b856a831fc16d35fd8f0f6906b2b618d
ryancdavey1 <ryancdavey1@gmail.com>
2020-10-19 18:07:18 -0600

    use address input from command line

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-forecast(-75.7088, 44.1545, (error, data) => {
-  console.log('Error', error);
-  console.log('Data', data)
-});
+const address = process.argv[2]
+
+if (!address) {
+  console.log("Please provide an address")
+} else {
+  geocode(address, (error, data) => {
+    if (error) {
+      return console.log(error)
+    } 
+    
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log('Error', error)
+      }
+      console.log(data.location)
+      console.log(forecastData)
+    })
+   })
+}
+ 
+
