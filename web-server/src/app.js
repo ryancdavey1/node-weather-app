ef13896108c70d03db3f231f9805f115e307b5b2
ryancdavey1 <ryancdavey1@gmail.com>
2020-11-18 22:06:38 -0700

    add handlebars templates and routes in app.js

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,17 +2,45 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'test'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'test'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'Text for the Help page.'
+    })
+})
+
 app.get('/weather', (req, res) => {
-  res.send({
-    forecast: "Snowing",
-    location: "Boston"
-  })
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
 })
 
 app.listen(3000, () => {
-  console.log('Server is up on port 3000.')
+    console.log('Server is up on port 3000.')
 })
