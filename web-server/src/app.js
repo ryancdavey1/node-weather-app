1c3b060983314d1b18ec09a737b210c4f87b12da
ryancdavey1 <ryancdavey1@gmail.com>
2020-11-19 23:50:34 -0700

    add header partial and partials folder

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
 const path = require('path')
 const express = require('express')
-
+const hbs = require('hbs')
+    
 const app = express()
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -17,20 +20,22 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'test'
+        name: 'test user'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'test'
+        name: 'test user'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'Text for the Help page.'
+        helpText: 'Text for the Help page.',
+        title: 'Help',
+        name: 'test user'
     })
 })
 
