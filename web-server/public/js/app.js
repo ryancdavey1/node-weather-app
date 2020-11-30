955319ce2fed95c74344e44084d4ad5395008d8c
ryancdavey1 <ryancdavey1@gmail.com>
2020-11-29 21:00:50 -0700

    add search form for /weather page to get forecast

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,22 +1,29 @@
 console.log('Client side javascript file is loaded')
 
-fetch('http://puzzle.mead.io/puzzle')
-  .then(response => {
-    return response.json()
-  })
-  .then(json => {
-    console.log(json)
-  })
+const weatherForm = document.querySelector("form")
+const search = document.querySelector("input")
+const locationMessage = document.querySelector("#location-message")
+const forecastMessage = document.querySelector("#forecast-message")
 
-fetch('http://localhost:3000/weather?address=boston')
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault() 
+
+  const location = search.value
+  locationMessage.textContent = "Loading..."
+  forecastMessage.textContent = ""
+
+  fetch(`http://localhost:3000/weather?address=${location}`)
   .then(response => {
     return response.json()
   })
   .then(json => {
     if (json.error) {
-      console.log(json.error)
+      locationMessage.textContent = json.error 
     } else {
-      console.log(json.location)
-      console.log(json.forecast)
+      locationMessage.textContent = json.location
+      forecastMessage.textContent = json.forecast
     }
   })
+})
+
+
