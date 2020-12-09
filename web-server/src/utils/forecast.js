adf02cbfa5e44b77bea1d9c528bced726785f10b
ryancdavey1 <ryancdavey1@gmail.com>
2020-12-08 23:46:52 -0700

    add humidity % to forecast

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -4,12 +4,15 @@ const forecast = (latitude, longitude, callback) => {
   const url = 'http://api.weatherstack.com/current?access_key=6070727b10470ab400ec81a3360c6c28&query=' + latitude + ',' + longitude + '&units=f'
 
   request({ url, json: true}, (error, {body}) => {
+  
     if (error) {
       callback('Unable to connect to weather service!', undefined)
     } else if (body.error) {
       callback('Unable to find location', undefined)
     } else {
-      callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`)
+      console.log(body.current);
+      callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. 
+      The humidity is ${body.current.humidity}%.`)
     }
   });
 };
